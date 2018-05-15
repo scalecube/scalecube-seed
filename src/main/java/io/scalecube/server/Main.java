a148ed6de08563cfd64c7294758a3208c95e064e
aharonha <aharon@marketspulse.com>
2018-05-15 18:22:46 +0300

    clean up code

--- src/main/java/io/scalecube/server/Main.java
+++ src/main/java/io/scalecube/server/Main.java
@@ -2,9 +2,6 @@ package io.scalecube.server;
 
 import io.scalecube.cluster.ClusterConfig;
 import io.scalecube.cluster.ClusterConfig.Builder;
-import io.scalecube.packages.utils.Cli;
-import io.scalecube.packages.utils.Logo;
-import io.scalecube.packages.utils.PackageInfo;
 import io.scalecube.services.Microservices;
 
 import java.util.Arrays;
@@ -31,17 +28,18 @@ public class Main {
     Builder conf = ClusterConfig.builder().metadata(map);
 
     if (packageInfo.seedAddress() == null) {
-      Cli.prln("Seed Host and port not specified - running as standalone seed.");
-      seed = Microservices.builder().servicePort(Microservices.SERVICE_PORT).clusterConfig(conf).build();
+      System.out.println("Seed Host and port not specified - running as standalone seed.");
+      seed = Microservices.builder().clusterConfig(conf).build();
     } else {
-      Cli.prln("Seed Host and port specified - trying to join cluster: " + Arrays.toString(packageInfo.seedAddress()));
-      seed = Microservices.builder().servicePort(Microservices.SERVICE_PORT).clusterConfig(conf)
-          .seeds(packageInfo.seedAddress()).build();
+      System.out.println(
+          "Seed Host and port specified - trying to join cluster: " + Arrays.toString(packageInfo.seedAddress()));
+      seed = Microservices.builder().clusterConfig(conf)
+          .seeds(packageInfo.seedAddress())
+          .build();
     }
 
     Logo.builder().tagVersion(packageInfo.version())
         .port(String.valueOf(seed.cluster().address().port()))
-        .header("Service Port: " + seed.serviceAddress().port())
         .ip(seed.cluster().address().host())
         .group(packageInfo.groupId())
         .artifact(packageInfo.artifactId())
@@ -50,11 +48,6 @@ public class Main {
         .pid(packageInfo.pid())
         .hostname(packageInfo.hostname())
         .website().draw();
-    
-    if (args.length > 0) {
-      Cli cli = new Cli(seed);
-      cli.start();
-    }
 
     Thread.currentThread().join();
   }
---
+++ src/main/java/io/scalecube/server/PackageInfo.java
@@ -0,0 +1,158 @@
+package io.scalecube.server;
+
+import io.scalecube.transport.Address;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Properties;
+import java.util.stream.Collectors;
+
+/**
+ * Information provider about the environment and the package of this instance.
+ *
+ */
+public class PackageInfo {
+
+  private final Properties properties = new Properties();
+
+  /**
+   * Runtime Environment information provider.
+   */
+  public PackageInfo() {
+    if (System.getenv("SC_HOME") != null) {
+      String path = System.getenv("SC_HOME");
+
+      try {
+        FileInputStream in = new FileInputStream(path + "package.properties");
+        properties.load(in);
+      } catch (IOException e) {
+        System.out.println("cannot open file: " + path + "package.properties cause:" + e.getCause());
+        defaultProps();
+      }
+    } else {
+      InputStream stream = PackageInfo.class.getResourceAsStream("package.properties");
+      if (stream != null) {
+        try {
+          properties.load(stream);
+        } catch (IOException e) {
+          defaultProps();
+        }
+      } else {
+        defaultProps();
+      }
+    }
+  }
+
+  private void defaultProps() {
+    properties.put("version", "Development");
+    properties.put("groupId", "Development");
+    properties.put("artifactId", "Development");
+    properties.put("version", "Development");
+    properties.put("version", "Development");
+  }
+
+  public String version() {
+    return properties.getProperty("version");
+  }
+
+  public String groupId() {
+    return properties.getProperty("groupId");
+  }
+
+  public String artifactId() {
+    return properties.getProperty("artifactId");
+  }
+
+  public String java() {
+    return System.getProperty("java.version");
+  }
+
+  public String os() {
+    return System.getProperty("os.name");
+  }
+
+  /**
+   * returns host name of the current running host.
+   * 
+   * @return host name.
+   */
+  public String hostname() {
+    String result = getVariable("HOSTNAME", "unknown");
+    if ("unknown".equals(result)) {
+      return getHostName("unknown");
+    } else {
+      return result;
+    }
+  }
+
+  private String getHostName(String defaultValue) {
+    String hostname = defaultValue;
+    try {
+      InetAddress addr = InetAddress.getLocalHost();
+      hostname = addr.getHostName();
+    } catch (UnknownHostException ex) {
+      hostname = defaultValue;
+    }
+    return hostname;
+  }
+
+  public String pid() {
+    String vmName = ManagementFactory.getRuntimeMXBean().getName();
+    int pids = vmName.indexOf("@");
+    String pid = vmName.substring(0, pids);
+    return pid;
+  }
+
+  /**
+   * resolves redis address.
+   */
+  public String redisAddress() {
+    return getVariable("REDIS_ADDRESS", "redis://localhost:6379");
+  }
+
+  /**
+   * Returns API Gateway API.
+   */
+  public int gatewayPort() {
+    String port = getVariable("API_GATEWAY_PORT", "8081");
+    return Integer.valueOf(port);
+  }
+
+  /**
+   * Resolve seed address from environment variable or system property.
+   * 
+   * @return seed address as string for example localhost:4801.
+   */
+  public Address[] seedAddress() {
+    String list = getVariable("SC_SEED_ADDRESS", null);
+    if (list != null && !list.isEmpty()) {
+      String[] hosts = list.split(",");
+      List<Address> seedList = Arrays.asList(hosts).stream().filter(predicate -> !predicate.isEmpty())
+          .map(mapper -> mapper.trim())
+          .map(hostAndPort -> {
+            return Address.from(hostAndPort);
+          }).collect(Collectors.toList());
+      return seedList.toArray(new Address[seedList.size()]);
+    } else {
+      return null;
+    }
+  }
+
+  private String getVariable(String name, String defaultValue) {
+    if (System.getenv(name) != null) {
+      return System.getenv(name);
+    }
+    if (System.getProperty(name) != null) {
+      return System.getProperty(name);
+    }
+    return defaultValue;
+  }
+
+
+}
